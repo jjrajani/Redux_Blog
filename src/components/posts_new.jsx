58a51c34b170c798a3caffb6183b4710e91ce76d
humdrum <jjrajani@gmail.com>
2017-08-13 19:01:12 -0400

    redux form for new posts

--- src/components/posts_new.jsx
+++ src/components/posts_new.jsx
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
 
 class PostsNew extends Component {
+
+  renderField(field) {
+    return (
+      <div className='form-group'>
+        <label>{field.label}</label>
+        <input
+          className='form-control'
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
 
   render() {
     return (
       <div>
         <div className="text-xs-right">
-          <Link className="btn btn-primary" to="/">Add A Post</Link>
+          <Link className="btn btn-primary" to="/">List Posts</Link>
         </div>
-        List Posts
+        <form>
+          <Field
+            label="Title"
+            name="title"
+            component={this.renderField}
+          />
+          <Field
+            label="Tags"
+            name="tags"
+            component={this.renderField}
+          />
+          <Field
+            label="Post Content"
+            name="content"
+            component={this.renderField}
+          />
+        </form>
       </div>
     );
   }
 
 }
-export default PostsNew;
+
+export default reduxForm({
+  form: 'PostsNewForm',
+})(PostsNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,15 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+
 import Posts from './reducer_posts';
 
 const rootReducer = combineReducers({
-  posts: Posts
+  posts: Posts,
+  form: formReducer,
 });
+
+/*
+  Import Reducer from Redux-Form Library
+*/
 
 export default rootReducer;
